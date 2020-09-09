7289683abe6e218adad7d51a57a2f06001a68a96
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-09-08 21:19:24 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res)=> res.json({message: 'API FUNCIONANDO'}))
