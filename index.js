de3adf620e1ee92300f58e6a80f8b6473551231f
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-09-08 22:25:29 -0500

    finish

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8877;
 app.use(express.json());
 
 app.get('/', (req, res)=> res.json({message: 'API FUNCIONANDO'}))
